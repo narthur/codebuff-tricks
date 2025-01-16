da472292f78d87b03ec048ed568ea9eeca8978c2
Nathan Arthur <nathan@nathanarthur.com>
2025-01-16 16:48:14 -0500

    simplify deploy configs

--- svelte.config.js
+++ svelte.config.js
@@ -3,21 +3,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			// Precompress assets
-			precompress: true,
-			// Fallback for SPA routing
-			fallback: 'index.html',
-			// Ensure strict paths for assets
-			strict: true
+			fallback: 'index.html'
 		})
 	}
 };
